a6a4bc67d3dd91c722bb439b9752ff4a2d0df556
hema omer <hema.omer007@gmail.com>
2026-02-01 02:47:42 +0300

    chore: Remove outputFileTracingExcludes configuration from next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingExcludes: {
-    '*': [
-      './public/data/scholars/**/*',
-      'public/data/scholars/**/*',
-    ],
-  },
 };
 
 export default withNextIntl(nextConfig);
